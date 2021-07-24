7cf6534eef25946820ecb81f196d1e1b32bb9507
hoobie <hoobie>
2021-07-24 14:07:57 +0800

    初始化,练习java8实战

---
+++ src/main/java/modernjavainaction/appa/Book.java
@@ -0,0 +1,18 @@
+package modernjavainaction.appa;
+
+
+import java.util.Arrays;
+
+@Author(name = "Raoul")
+@Author(name = "Mario")
+@Author(name = "Alan")
+public class Book {
+
+    public static void main(String[] args) {
+        Author[] authors = Book.class.getAnnotationsByType(Author.class);
+        Arrays.asList(authors).forEach(a -> {
+            System.out.println(a.name());
+        });
+    }
+
+}
